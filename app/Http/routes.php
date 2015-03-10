a11e0c82d0e641755ff25c26855c9256df1f7f7d
王颖伟 <ctw.wyw@gmail.com>
2015-03-10 15:05:16 +0800

    test webhook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 #Route::get('/', 'WelcomeController@index');
 Route::get('/', function() {
-    return "Hello World";
+    return "Test WebHook";
 });
 
 Route::get('home', 'HomeController@index');
